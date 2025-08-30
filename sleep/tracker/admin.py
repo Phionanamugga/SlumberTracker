a4687b23d5f3d7427c6a5e7a34254ffdc8de8f61
Phionanamugga <phionanamugga15@gmail.com>
2025-08-30 14:52:24 +0200

    modifies admin file

--- sleep/tracker/admin.py
+++ sleep/tracker/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+
 
 # Register your models here.
+from django.contrib import admin
+from .models import SleepSession
+
+@admin.register(SleepSession)
+class SleepAdmin(admin.ModelAdmin):
+    list_display = ("user", "start_time", "end_time", "duration_hours")
+    search_fields = ("user__username",)
+    list_filter = ("user",)
